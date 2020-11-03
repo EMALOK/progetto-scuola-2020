4f416f7ab47326db4a32ee2d53f4ff47ed5e8e1a
Tommaso Goni <tommasogo99@gmail.com>
2020-11-03 19:56:33 +0100

    Mosso GameClass.hpp nella corretta cartella

---
+++ headers/GameClass.hpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+private:
+    sf::RenderWindow *window; //oggetto della finestra (puntatore)
+    sf::VideoMode videomode;
+    sf::Event event;
+
+    //funzioni private
+    void initVars();
+    void initWindow();
+
+public:
+    //costruttore e distruttore
+    Game();
+    ~Game();
+    
+    //funzioni
+    void update(sf::Time);
+    void render();
+
+    bool isGameOpen();
+    void pollEvents();
+};
