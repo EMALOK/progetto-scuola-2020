febc38a7d14bc5e8cef8dbf7fb1d8a7df1a3e654
Tommaso Goni <tommasogo99@gmail.com>
2020-11-08 15:54:28 +0100

    Aggiunte le basi dell'implementazione
    - Aggiunta la documentazione
    
    - Aggiunte le funzioni di base (da scrivere)

--- MovingPlatform.cpp
+++ MovingPlatform.cpp
@@ -1,2 +1,70 @@
 #include "headers/MovingPlatform.hpp"
 
+//  Getters
+
+/**
+ * Restituiscre una copia della posizione di partenza
+ * 
+ * @return Copia della posizione di partenza
+ */
+sf::Vector2f MovingPlatform::getStartingPosition()
+{
+    //  TODO
+}
+
+/**
+ * Restituiscre una copia della posizione di arrivo
+ * 
+ * @return Copia della posizione di arrivo
+ */
+sf::Vector2f MovingPlatform::getEndingPosition()
+{
+    //  TODO
+}
+
+//  Update / Render
+
+/**
+ * Renderizzazione della piattaforma
+ * 
+ * @param window Finestra su cui applicare la renderizzazione
+ */
+void MovingPlatform::render(sf::RenderWindow* window)
+{
+    //  TODO
+}
+
+/**
+ * Update della posizione della piattaforma
+ * 
+ * @param delta_time Tempo passato dall'ultima chiamata
+ */
+void MovingPlatform::update(sf::Time delta_time)
+{
+    //  TODO
+}
+
+//  Costruttore e Distruttore
+
+/**
+ * Piattaforma mobile
+ * 
+ * @param starting_position Posizione di partenza
+ * @param ending_position Posizione di arrivo
+ * @param size Dimensioni
+ * @param texture_path Percorso file della texture
+ * @param animation_speed Velocità dell'animazione
+ * @param animation_time_offset Offset temporale dell'animazione
+ */
+MovingPlatform::MovingPlatform(sf::Vector2f starting_position, sf::Vector2f ending_position, sf::Vector2f size, sf::String texture_path, float animation_speed, sf::Time animation_time_offset)
+{
+    // TODO
+}
+
+/**
+ * Distruttore
+ */ 
+MovingPlatform::~MovingPlatform()
+{
+
+}
