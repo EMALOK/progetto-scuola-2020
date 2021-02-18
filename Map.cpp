b6d60dd62a46276520cd57d9b8423a44cc3fe9e4
Tommaso Goni <tommasogo99@gmail.com>
2021-02-18 23:25:42 +0100

    Formatted Map class and fixed include bugs

--- Map.cpp
+++ Map.cpp
@@ -2,7 +2,6 @@
 #include "headers/Map.hpp"
 #include <iostream> //Debug
 
-
 //Trova la x e la y del blocco 'Index' nel file json
 sf::Vector2f Map::posBlock(int Index)
 {
@@ -25,4 +24,13 @@ sf::Vector2f Map::posCoin(int Index)
 sf::Vector2f Map::dimCoin(int Index)
 {
     return sf::Vector2f(mapDoc["monete"][Index]["w"].GetInt(), mapDoc["monete"][Index]["h"].GetInt());
+}
+
+Map::Map(const char *map)
+{
+    FILE *fp = fopen(map, "rb");                           //Leggo il file
+    char buf[0XFFFF];                                      //inizializzo un carattere buffering corrispondente al numero di righe da analizzare (In questo caso altissimo)
+    rapidjson::FileReadStream input(fp, buf, sizeof(buf)); //Leggo lo stream
+    mapDoc.ParseStream(input);                             //Lo analizzo
+    fclose(fp);                                            //Chiudo il file
 }
--- headers/Map.hpp
+++ headers/Map.hpp
@@ -1,30 +1,22 @@
 #include <SFML/Graphics.hpp>
-#include <document.h> //Creare il documento da "analizzare"
-#include <filereadstream.h> //Leggere lo stream nel file json
-#include <fstream> //Leggere e chiudere il file json
+#include "rapidJson/document.h"       //Creare il documento da "analizzare"
+#include "rapidJson/filereadstream.h" //Leggere lo stream nel file json
+#include <fstream>                    //Leggere e chiudere il file json
 #include <iostream>
 
 class Map
 {
 public:
-    Map() { }
-
-    Map(const char* map)
-    {
-        FILE *fp = fopen(map, "rb"); //Leggo il file
-        char buf[0XFFFF];   //inizializzo un carattere buffering corrispondente al numero di righe da analizzare (In questo caso altissimo)
-        rapidjson::FileReadStream input(fp, buf, sizeof(buf)); //Leggo lo stream
-        mapDoc.ParseStream(input); //Lo analizzo
-        fclose(fp); //Chiudo il file
-    }
-
-    //Metodi
-
+    //  Metodi oggetti solidi
     sf::Vector2f posBlock(int Index);
     sf::Vector2f dimBlock(int Index);
-
+    //  Metodi monete
     sf::Vector2f posCoin(int Index);
     sf::Vector2f dimCoin(int Index);
 
     rapidjson::Document mapDoc;
+
+    //  Costrutte e Distruttore
+    Map(const char *map);
+    ~Map();
 };
